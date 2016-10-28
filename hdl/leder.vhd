470845317c98de218c84071424a1f21a74ccfb2b
pawelstrzempek <pawel.strzempek@uj.edu.pl>
2016-10-28 14:15:28 +0200

    pawel1

--- hdl/leder.vhd
+++ hdl/leder.vhd
@@ -15,7 +15,7 @@
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
---sim changes 
+--simulate changes 
 ----------------------------------------------------------------------------------
 
 
